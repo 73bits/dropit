6e1cca29f153d9292f093a7a9f9ddab45eaadad3
73bits <prashantkr0628@gmail.com>
2026-01-17 19:46:04 +0530

    add Create service

--- internal/service/text_service.go
+++ internal/service/text_service.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"errors"
+	"time"
+
+	"github.com/73bits/dropit/internal/model"
 	"github.com/73bits/dropit/internal/repo"
+	"github.com/73bits/dropit/internal/util"
 )
 
 type TextService struct {
@@ -10,4 +15,19 @@ type TextService struct {
 
 func NewTextService(r repo.TextRepo) *TextService {
 	return &TextService{repo: r}
+}
+
+func (s *TextService) Create(content string, ttlseconds int) (model.Text, error) {
+	if content == "" || ttlseconds <= 0 {
+		return model.Text{}, errors.New("invalid input")
+	}
+
+	text := model.Text{
+		ID:        util.GenerateID(),
+		Content:   content,
+		ExpiresAt: time.Now().Add(time.Duration(ttlseconds) * time.Second),
+	}
+
+	err := s.repo.Save(text)
+	return text, err
 }
