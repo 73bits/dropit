c1a19424f3bd006af01bba3148f7912512500061
73bits <prashantkr0628@gmail.com>
2026-01-18 18:25:56 +0530

    add Create handler for new text with ttl_seconds

--- internal/handler/text.go
+++ internal/handler/text.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/73bits/dropit/internal/service"
 )
 
@@ -10,4 +13,25 @@ type TextHandler struct {
 
 func NewTextHandler(s *service.TextService) *TextHandler {
 	return &TextHandler{service: s}
+}
+
+func (h *TextHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Content    string `json:"content"`
+		TTLSeconds int    `json:"ttl_seconds"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	text, err := h.service.Create(req.Content, req.TTLSeconds)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"url": "http://" + r.Host + "/text/" + text.ID,
+	})
 }
